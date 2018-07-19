c2bdc8241d4fd1e67efb612b6143f4b40fab8e7d
Rafael Rodrigues <rafaelpascoalrodrigues@gmail.com>
2018-07-19 15:26:58 -0300

    Retrieve data from Entities

--- ghost_in_the_cell/code.py
+++ ghost_in_the_cell/code.py
@@ -1,28 +1,43 @@
 import sys
 import math
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
 
-factory_count = int(input())  # the number of factories
-link_count = int(input())  # the number of links between factories
+# initialize factory list
+factory_count = int(input())
+factory_list = dict()
+for i in range(factory_count):
+    factory_list[i] = {
+        'player' : 0,
+        'population' : 0,
+        'production' : 0,
+        'links' : [{
+            'distance' : -1
+        } for i in range(factory_count)],
+    }
+
+
+# setup distance between factories
+link_count = int(input())
 for i in range(link_count):
     factory_1, factory_2, distance = [int(j) for j in input().split()]
+    factory_list[factory_1]['links'][factory_2]['distance'] = distance
+    factory_list[factory_2]['links'][factory_1]['distance'] = distance
+
 
 # game loop
 while True:
-    entity_count = int(input())  # the number of entities (e.g. factories and troops)
+    # retrieve data from entities
+    entity_count = int(input())
     for i in range(entity_count):
         entity_id, entity_type, arg_1, arg_2, arg_3, arg_4, arg_5 = input().split()
-        entity_id = int(entity_id)
-        arg_1 = int(arg_1)
-        arg_2 = int(arg_2)
-        arg_3 = int(arg_3)
-        arg_4 = int(arg_4)
-        arg_5 = int(arg_5)
 
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr)
+        if entity_type == 'FACTORY':
+            entity = factory_list[int(entity_id)]
+            entity['player'] = int(arg_1)
+            entity['population'] = int(arg_2)
+            entity['production'] = int(arg_3)
+            int(arg_4)
+            int(arg_5)
 
 
     # Any valid action, such as "WAIT" or "MOVE source destination cyborgs"
